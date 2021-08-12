b39417e0fd7ca72508cabd33ef65cc371a147868
ShaikTaha1 <shaiktaha14@gmail.com>
2021-08-12 08:15:23 +0000

    Temporary changes

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = new express();
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+function getNLUInstance(){
+let api_key = process.env.MyTuWwuajgb2VIjk7HLVeMKcy2yVmy-KQ9gArZqMPjCt;
+let api_url = process.env.https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/fd5fcac8-f166-4e16-a598-9fe8deda7973;
+
+const NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-understanding/v1");
+const { IamAuthenticator } = require("ibm-watson/auth');
+
+const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+version: '2021-08-01',
+  authenticator: new IamAuthenticator({
+    apikey: '{apikey}',
+}),
+serviceUrl: api_url,
+});
+
+return naturalLanguageUnderstanding;
+}
 app.use(express.static('client'))
 
 const cors_app = require('cors');
